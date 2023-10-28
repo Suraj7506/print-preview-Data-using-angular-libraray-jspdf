8faa7f65e7166c6267b39adf23250dee5b09ab87
suraj shukla <sdshukla270@gmail.com>
2023-10-28 18:42:42 +0530

    changes

--- src/app/directive/export/export-pdf.directive.ts
+++ src/app/directive/export/export-pdf.directive.ts
@@ -8,48 +8,37 @@ import 'jspdf-autotable';
 export class ExportPdfDirective {
   @Input() contentId!: string;
   @Input() jsonData: any[] = [];
-
   @HostListener('click')
   onClick() {
     const source = document.getElementById(this.contentId);
     if (source) {
-      // Explicitly set the page size to A4
       const doc = new jsPDF({
-        unit: 'px',
-        format: 'a4',
+        unit: 'mm',
+        format: [300,320],
         orientation: 'p',
       });
       let pageTopMargin = 20;
       const pageNumbersY = doc.internal.pageSize.height - 10;
       const contentHeight = doc.internal.pageSize.height - pageTopMargin - pageNumbersY;
-
       doc.html((document?.getElementById(this.contentId) as HTMLElement), {
         callback: function (doc) {
           const totalPages = doc.getNumberOfPages();
-
           for (let i = 1; i <= totalPages; i++) {
             doc.setPage(i);
-            doc.text("Company Name: Nimap", 120, pageTopMargin);
+            doc.text("Company Name: Nimap", 50, pageTopMargin);
             pageTopMargin = 20;
-
             const currentPageHeight = i === totalPages ? doc.internal.pageSize.height - pageTopMargin : contentHeight;
-
-            // Calculate if there is enough space on the current page
             const usedSpace = pageTopMargin + currentPageHeight;
             const remainingSpace = doc.internal.pageSize.height - usedSpace;
-
-            // If there isn't enough space, add a new page
             if (remainingSpace < 0 && i < totalPages) {
               doc.addPage();
               pageTopMargin = 20;
             }
-
             doc.text(`Page ${i} of ${totalPages}`, doc.internal.pageSize.width / 2, pageNumbersY, {
               align: 'center',
             });
           }
-
-          doc.save();
+          doc.save("a4.pdf");
         },
       });
     } else {

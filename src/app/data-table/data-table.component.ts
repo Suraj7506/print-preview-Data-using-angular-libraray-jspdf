370a081fff1480782343d681c78198708e54c78a
suraj shukla <sdshukla270@gmail.com>
2023-10-30 15:35:00 +0530

    changes

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -13,105 +13,233 @@
       company = "Your Company"; // Sample value, set to your desired value
       address = "123 Main Street"; 
 
-      public myData: any[] = [
+      // public myData: any[] = [
+      //   {
+      //     sectionTitle: 'Journal de caisse MyOrders du 01/01/2020',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Nguyen', lastName: 'Dai' },
+      //       { no: 2, firstName: 'John', lastName: 'Doe' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      //   {
+      //     sectionTitle: 'tva repartition on PAYGREEN payment',
+      //     headers: ['No.', 'FirstName', 'Last Name'],
+      //     items: [
+      //       { no: 1, firstName: 'Alice', lastName: 'Smith' },
+      //       { no: 2, firstName: 'Bob', lastName: 'Johnson' },
+      //       // Add more data as needed
+      //     ]
+      //   },
+      // ];
+    
+      USERS = [
         {
-          sectionTitle: 'Journal de caisse MyOrders du 01/01/2020',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Nguyen', lastName: 'Dai' },
-            { no: 2, firstName: 'John', lastName: 'Doe' },
-            // Add more data as needed
-          ]
+          "id": 1,
+          "name": "Leanne Graham",
+          "email": "sincere@april.biz",
+          "phone": "1-770-736-8031 x56442"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 2,
+          "name": "Ervin Howell",
+          "email": "shanna@melissa.tv",
+          "phone": "010-692-6593 x09125"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 3,
+          "name": "Clementine Bauch",
+          "email": "nathan@yesenia.net",
+          "phone": "1-463-123-4447",
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 4,
+          "name": "Patricia Lebsack",
+          "email": "julianne@kory.org",
+          "phone": "493-170-9623 x156"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 5,
+          "name": "Chelsey Dietrich",
+          "email": "lucio@annie.ca",
+          "phone": "(254)954-1289"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 6,
+          "name": "Mrs. Dennis",
+          "email": "karley@jasper.info",
+          "phone": "1-477-935-8478 x6430"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 7,
+          "name": "Leanne Graham",
+          "email": "sincere@april.biz",
+          "phone": "1-770-736-8031 x56442"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 8,
+          "name": "Ervin Howell",
+          "email": "shanna@melissa.tv",
+          "phone": "010-692-6593 x09125"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 9,
+          "name": "Clementine Bauch",
+          "email": "nathan@yesenia.net",
+          "phone": "1-463-123-4447",
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 10,
+          "name": "Patricia Lebsack",
+          "email": "julianne@kory.org",
+          "phone": "493-170-9623 x156"
         },
         {
-          sectionTitle: 'tva repartition on PAYGREEN payment',
-          headers: ['No.', 'FirstName', 'Last Name'],
-          items: [
-            { no: 1, firstName: 'Alice', lastName: 'Smith' },
-            { no: 2, firstName: 'Bob', lastName: 'Johnson' },
-            // Add more data as needed
-          ]
+          "id": 11,
+          "name": "Chelsey Dietrich",
+          "email": "lucio@annie.ca",
+          "phone": "(254)954-1289"
         },
+        {
+          "id": 12,
+          "name": "Mrs. Dennis",
+          "email": "karley@jasper.info",
+          "phone": "1-477-935-8478 x6430"
+        },
+        {
+          "id": 13,
+          "name": "Leanne Graham",
+          "email": "sincere@april.biz",
+          "phone": "1-770-736-8031 x56442"
+        },
+        {
+          "id": 14,
+          "name": "Ervin Howell",
+          "email": "shanna@melissa.tv",
+          "phone": "010-692-6593 x09125"
+        },
+        {
+          "id": 15,
+          "name": "Clementine Bauch",
+          "email": "nathan@yesenia.net",
+          "phone": "1-463-123-4447",
+        },
+        {
+          "id": 16,
+          "name": "Patricia Lebsack",
+          "email": "julianne@kory.org",
+          "phone": "493-170-9623 x156"
+        },
+        {
+          "id": 17,
+          "name": "Chelsey Dietrich",
+          "email": "lucio@annie.ca",
+          "phone": "(254)954-1289"
+        },
+        {
+          "id": 18,
+          "name": "Mrs. Dennis",
+          "email": "karley@jasper.info",
+          "phone": "1-477-935-8478 x6430"
+        },
+        {
+          "id": 19,
+          "name": "Leanne Graham",
+          "email": "sincere@april.biz",
+          "phone": "1-770-736-8031 x56442"
+        },
+        {
+          "id": 20,
+          "name": "Ervin Howell",
+          "email": "shanna@melissa.tv",
+          "phone": "010-692-6593 x09125"
+        },
+        
       ];
+    
+    
+    
+    
     }    
--- src/app/directive/export/export-pdf.directive.ts
+++ src/app/directive/export/export-pdf.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, Input, HostListener, ElementRef } from '@angular/core';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
+import html2canvas from 'html2canvas';
+
 
 @Directive({
   selector: '[appExportPdf]'
@@ -10,41 +12,66 @@ export class ExportPdfDirective {
   @Input() jsonData: any[] = [];
   @HostListener('click')
   onClick() {
-    const source = document.getElementById(this.contentId);
-    if (source) {
-      const doc = new jsPDF({
-        unit: 'mm',
-        format: 'a4',
-        orientation: 'p',
-      });
-      let pageTopMargin = 20;
-      const pageNumbersY = doc.internal.pageSize.height - 10;
-      const contentHeight = doc.internal.pageSize.height - pageTopMargin - pageNumbersY;
-      doc.html((document?.getElementById(this.contentId) as HTMLElement), {
-        callback: function (doc) {
-          const totalPages = doc.getNumberOfPages();
-          for (let i = 1; i <= totalPages; i++) {
-            doc.setPage(i);
-            doc.text("Company Name: Nimap", 50, pageTopMargin);
-            pageTopMargin = 20;
-            const currentPageHeight = i === totalPages ? doc.internal.pageSize.height - pageTopMargin : contentHeight;
-            const usedSpace = pageTopMargin + currentPageHeight;
-            const remainingSpace = doc.internal.pageSize.height - usedSpace;
-            if (remainingSpace < 0 && i < totalPages) {
-              doc.addPage();
-              pageTopMargin = 20;
-            }
-            doc.text(`Page ${i} of ${totalPages}`, doc.internal.pageSize.width / 2, pageNumbersY, {
-              align: 'center',
-            });
-          }
-          doc.save("a4.pdf");
-        },
-      });
-    } else {
-      console.error(`Element with id '${this.contentId}' not found.`);
-    }
+    this.captureContent().then((pages) => {
+      this.generatePDF(pages);
+    });
   }
 
+  private captureContent(): Promise<HTMLCanvasElement[]> {
+    return new Promise((resolve) => {
+      const DATA: any = document.getElementById(this.contentId);
+      DATA.scrollTop = 0;
 
+      html2canvas(DATA, { scrollY: -window.scrollY }).then((canvas) => {
+        const pageHeight = 700; // A4 page height in mm
+        const headerHeight = 20; // Header height in mm
+        const footerHeight = 80; // Footer height in mm
+
+        const totalPages = Math.ceil(canvas.height / (pageHeight - headerHeight - footerHeight));
+
+        const pages: HTMLCanvasElement[] = [];
+
+        for (let i = 0; i < totalPages; i++) {
+          const PAGE_CANVAS: HTMLCanvasElement = document.createElement('canvas');
+          const pageContext = PAGE_CANVAS.getContext('2d');
+
+          if (pageContext) {
+            PAGE_CANVAS.width = canvas.width;
+            PAGE_CANVAS.height = pageHeight;
+
+            const startY = i * (pageHeight - headerHeight - footerHeight);
+            const endY = Math.min((i + 1) * (pageHeight - headerHeight - footerHeight), canvas.height);
+            const height = endY - startY;
+
+            pageContext.drawImage(canvas, 0, startY, canvas.width, height, 0, 0, canvas.width, height);
+
+            pages.push(PAGE_CANVAS);
+          }
+        }
+
+        resolve(pages);
+      });
+    });
+  }
+
+  private generatePDF(pages: HTMLCanvasElement[]) {
+    const fileWidth = 600; // A4 page width in mm
+    const headerHeight = 20; // Header height in mm
+    const footerHeight = 10; // Footer height in mm
+    const PDF = new jsPDF('p', 'mm', 'a4');
+    for (let i = 0; i < pages.length; i++) {
+      const canvas = pages[i];
+      const fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      if (i > 0) {
+        PDF.addPage();
+      }
+      const pageHeight = fileHeight - headerHeight - footerHeight; // Adjust for header and footer
+      const footerY = pageHeight + headerHeight; // Position the footer at the bottom of the page
+      PDF.addImage(FILEURI, 'PNG', 0, headerHeight, fileWidth, pageHeight);
+      PDF.text('Nimap Infotech', 100, 10); // Header content
+      PDF.text('LLP Baniya', 100, footerY - 2); // Footer content
+    }
+    PDF.save('angular-demo.pdf');
+  }
 }
